04ae9d81648725c6c6cd3fa2b6f782a51d6d5717
suuhyeony <vv5835@naver.com>
2021-03-04 19:54:19 +0900

    complete

--- Infinite scroll/script.js
+++ Infinite scroll/script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('.container');
+const loading = document.querySelector('.loading');
 
 getPosts();
 getPosts();
 getPosts();
 
+
+// 포스트 불러오기
 async function getPosts () {
     const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${getRandomNr()}`);
     const postData = await postResponse.json();
@@ -14,16 +17,37 @@ async function getPosts () {
     const userResponse = await fetch('https://randomuser.me/api');
     const userData = await userResponse.json();
 
-    console.log(userData);
+    // console.log(userData);
 
     const data = { post: postData, user: userData.results[0]};
     printData(data);
 }
 
+
+// id 랜덤으로 불러오기 (1-100)
 function getRandomNr() {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+
+// scroll 이벤트
+window.addEventListener('scroll', () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    console.log({scrollTop, scrollHeight, clientHeight});
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+        showLoading();
+    }
+})
+
+
+// loading animation
+function showLoading() {
+    loading.classList.add('show');
+    setTimeout(getPosts, 1000);
+}
+
+
+// DOM에 데이터 넣기
 function printData(data) {
     const postContainer = document.createElement('div');
     postContainer.classList.add('posts-container');
@@ -36,6 +60,7 @@ function printData(data) {
         </div>
     `;
     container.appendChild(postContainer);
+    loading.classList.remove('show');
 }
 
 
